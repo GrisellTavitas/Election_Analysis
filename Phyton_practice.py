63150a6cdc20a905ff66bad392cd76a2d06e185a
GrisellTavitas <griselltavitasm@gmail.com>
2021-10-08 23:20:14 -0500

    Adding the election audit code

--- Phyton_practice.py
+++ Phyton_practice.py
@@ -196,5 +196,5 @@
 #     print(county + " county has " + str(voters) + " registered voters.")
 
 voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
-for county, registered_voters in range(len(voting_data)):
-    print(f'{county} county has {registered_voters} registered voters.')
+for county, registered_voters in voting_data[]:
+    print(f"{county} county has {registered_voters} registered voters.")
